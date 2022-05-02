0e83cf1496f18a9d5747a2052c187db095b097bb
Jessica-Lynn3 <jesslynn19338@gmail.com>
2022-05-02 14:59:23 -0700

    Melon Delivery Report Completed-- Reduced Repetition, Added Comments

--- produce_summary.py
+++ produce_summary.py
@@ -5,43 +5,45 @@ INSTRUCTIONS:
         - Walk through the code and add comments explaining each line of code as well as you can. """
 
 
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# Create a function to reduce repetition
+# Our function will take in 2 arguments- the day_number and the text file for that day
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+def get_daily_delivery_info(day_number, file_for_that_day):
+    """ Prints melon delivery info for day number entered and the text file passed in.
+    
+    States day number, opens the text file, reads through each line and prints report. 
+    
+    Report dislplays:
+        - number delivered of the melon type, 
+        - specific name of that melon type,
+        - and the total amount in sales for the melon type."""
+    
+    # Helpful to print a line of dashes-- in terminal this quickly indicates to reader a separate section
+    #  f-string prints the start of report and day_number (that will be passed in when the function is called)  
+    print("-" * 35 + f"START OF DAY {day_number} REPORT" + "-" * 35)
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+    #   created a variable that opens the text file (that will be passed in when the function is called)
+    file_open = open(file_for_that_day)
 
+    #   started a for loop to iterate through each line of the text file
+    for line in file_open:          #   for each line of the file passed in, 
+        line = line.rstrip()        #   remove the white space to the right for each line, and assign this to the variable line
+        words = line.split('|')     #   split line by the | sign, which creates a list; assign this to the variable words
 
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        melon = words[0]            #   assign the variable melon to index 0 of words
+        count = words[1]            #   assign the variable count to index 1 of words
+        amount = words[2]           #   assign the variable amount to index 2 of words
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        # for each line, we print out information for our report (using an f-string and the variables melon, count and amount)
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    
+    #   close the text file
+    file_open.close()
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+    #  f-string prints the end of report and day_number so reader knows report is finished for that day
+    print("-" * 35 + f"END OF DAY {day_number} REPORT" + "-" * 35)
 
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+#   here we call our functions, passing in different arguments with each function call
+get_daily_delivery_info(1, "um-deliveries-day-1.txt")
+get_daily_delivery_info(2, "um-deliveries-day-2.txt")
+get_daily_delivery_info(3, "um-deliveries-day-3.txt")
